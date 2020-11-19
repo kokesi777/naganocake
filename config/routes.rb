283a8a53404eb5b6b6e3301b8a2290df187ead3d
vagrant <vagrant@localhost.localdomain>
2020-11-19 13:03:03 +0000

    [add]admin/genres

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,12 @@
 Rails.application.routes.draw do
-  devise_for :admins
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root 'admin/homes#top'
+  
+  #admin
+  devise_for :admins  
 
-    #admin
     namespace :admin do
+      root 'homes#top'
+      root 'genres#index'
       resources :homes
     end
 
