f9747825c0d3ba177764cca37d147002e1f090dd
vagrant <vagrant@localhost.localdomain>
2020-11-19 12:14:13 +0000

    [add]admin/homes

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,11 @@
 Rails.application.routes.draw do
   devise_for :admins
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root 'admin/homes#top'
+
+    #admin
+    namespace :admin do
+      resources :homes
+    end
+
 end
