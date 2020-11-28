4d3bd557a1e627ef4e4ba33be92b8596cd553d27
vagrant <vagrant@localhost.localdomain>
2020-11-28 14:20:50 +0000

    [add]public/device

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,27 @@
 Rails.application.routes.draw do
+  #public
+    devise_for :publics, controllers: {
+      sessions:      'publics/sessions',
+      passwords:     'publics/passwords',
+      registrations: 'publics/registrations'
+  }
+
   namespace :public do
     get 'homes/top'
     get 'homes/about'
   end
+
+  root to: "public/homes#top"
+  get "/about" => "public/homes#about"
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   
   #admin
-  devise_for :admin
+  devise_for :admins, controllers: {
+    sessions:      'admins/sessions',
+    passwords:     'admins/passwords',
+    registrations: 'admins/registrations'
+}
 
   namespace :admin do
     resources :homes
@@ -18,13 +33,6 @@ Rails.application.routes.draw do
     resources :items
     post 'items/new' => 'items#create'
     get 'items/:id' => 'items#show'
-  end
-
-  #public
-  root to: "public/homes#top"
-  get "/about" => "public/homes#about"
-
-  namespace :public do
   end
 
 end
--- db/migrate/20201128130740_devise_create_publics.rb
+++ db/migrate/20201128130740_devise_create_publics.rb
@@ -1,8 +1,8 @@
 # frozen_string_literal: true
 
-class DeviseCreateAdmins < ActiveRecord::Migration[5.2]
+class DeviseCreatePublics < ActiveRecord::Migration[5.2]
   def change
-    create_table :admins do |t|
+    create_table :publics do |t|
       ## Database authenticatable
       t.string :email,              null: false, default: ""
       t.string :encrypted_password, null: false, default: ""
@@ -32,13 +32,13 @@ class DeviseCreateAdmins < ActiveRecord::Migration[5.2]
       # t.string   :unlock_token # Only if unlock strategy is :email or :both
       # t.datetime :locked_at
 
-      t.string :name  null: false
+      t.string :name
       t.timestamps null: false
     end
 
-    add_index :admins, :email,                unique: true
-    add_index :admins, :reset_password_token, unique: true
-    # add_index :admins, :confirmation_token,   unique: true
-    # add_index :admins, :unlock_token,         unique: true
+    add_index :publics, :email,                unique: true
+    add_index :publics, :reset_password_token, unique: true
+    # add_index :publics, :confirmation_token,   unique: true
+    # add_index :publics, :unlock_token,         unique: true
   end
 end
