1731b10001f5086ad38bdfab6356836efc57f3f6
vagrant <vagrant@localhost.localdomain>
2020-12-12 09:09:30 +0000

    [add]adminuserpage

--- app/controllers/admin/end_users_controller.rb
+++ app/controllers/admin/end_users_controller.rb
@@ -1,8 +1,10 @@
 class Admin::EndUsersController < ApplicationController
   def index
+    @emd_users = Enduser,all
   end
 
   def show
+    @end_user = EndUser.find(params[:id])
   end
 
   def edit
