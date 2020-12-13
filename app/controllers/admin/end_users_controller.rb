be34fde174943004a2cc0c86de28f1fdae9f4e89
vagrant <vagrant@localhost.localdomain>
2020-12-13 04:47:29 +0000

    [add]editshow

--- app/controllers/admin/end_users_controller.rb
+++ app/controllers/admin/end_users_controller.rb
@@ -1,6 +1,6 @@
 class Admin::EndUsersController < ApplicationController
   def index
-    @emd_users = Enduser,all
+    @emd_users = Enduser.all
   end
 
   def show
@@ -8,8 +8,12 @@ class Admin::EndUsersController < ApplicationController
   end
 
   def edit
+    @end_user = EndUser.find(params[:id])
+    @end_user.save
+    redirect_to admin_end_user_path
   end
 
   def update
+    @book = Book.find(params[:id])
   end
 end
