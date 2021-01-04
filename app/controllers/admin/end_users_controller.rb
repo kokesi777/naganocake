f50a0c8b3de9c87aeebaa126487049f8a8e20366
vagrant <vagrant@localhost.localdomain>
2021-01-04 07:53:23 +0000

    [fix]enduser

--- app/controllers/admin/end_users_controller.rb
+++ app/controllers/admin/end_users_controller.rb
@@ -1,19 +1,19 @@
 class Admin::EndUsersController < ApplicationController
   def index
-    @end_users = Enduser.all
+    @end_users = EndUser.all
   end
 
   def show
-    @end_user = Enduser.find(params[:id])
+    @end_user = EndUser.find(params[:id])
   end
 
   def edit
-    @end_user = Enduser.find(params[:id])
+    @end_user = EndUser.find(params[:id])
     @end_user.save
     redirect_to admin_end_user_path
   end
 
   def update
-    @end_user = Enduser.find(params[:id])
+    @end_user = EndUser.find(params[:id])
   end
 end
--- app/controllers/public/end_users_controller.rb
+++ app/controllers/public/end_users_controller.rb
@@ -1,4 +1,6 @@
 class Public::EndUsersController < ApplicationController
+    befor_action :login_required
+    
     def show
         @end_user = EndUser.find(params[:id])
     end
@@ -7,6 +9,11 @@ class Public::EndUsersController < ApplicationController
     end
         
     def unsubscribe
+    end
+
+    private
+    def login_required
+        redirect_to root_path
     end
 
 end
