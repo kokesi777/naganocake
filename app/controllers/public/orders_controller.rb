bccc0012540f6b3440d7dea45c9a9a534950849a
vagrant <vagrant@localhost.localdomain>
2021-01-24 13:51:38 +0000

    [fix]confirmpage

--- app/controllers/public/orders_controller.rb
+++ app/controllers/public/orders_controller.rb
@@ -8,7 +8,23 @@ class Public::OrdersController < ApplicationController
 
   def confirm
     @cart_items = current_end_user.cart_items
-    @order = Order.new(order_params)
+    @order = Order.new
+    if params[:address_num] == "1"
+      @order.post_code = current_end_user.post_code
+      @order.address = current_end_user.address
+      @order.name = current_end_user.first_name + current_end_user.last_name
+    elsif params[:address_num] == "2"
+      address = Address.find(params[:order][:address])
+      @order.post_code = address.post_code
+      @order.address = address.address
+      @order.name = address.name
+    elsif params[:address_num] == "3"
+      @order.post_code = params[:order][:post_code]
+      @order.address = params[:order][:address]
+      @order.name = params[:order][:name]
+    else
+      render 'new'
+    end
     @sum = 0
   end
 
@@ -26,11 +42,13 @@ class Public::OrdersController < ApplicationController
   end
 
   def show
+    @order = Order.find(params[:id])
+    @sum = 0
   end
 
 
   private
    def order_params
-    params.permit(:post_code, :name, :address)
+    params.permit(:post_code, :name, :address, :payment_method)
    end
 end
--- app/models/order.rb
+++ app/models/order.rb
@@ -1,4 +1,8 @@
 class Order < ApplicationRecord
     has_many :end_users
     has_many :items
+
+    enum status: {
+
+    }
 end
