018f53f452685fa974791cbacdf7f420c32fbb0b
vagrant <vagrant@localhost.localdomain>
2021-02-06 14:43:40 +0000

    [fix]a-endusereditpage

--- app/controllers/admin/end_users_controller.rb
+++ app/controllers/admin/end_users_controller.rb
@@ -15,7 +15,12 @@ class Admin::EndUsersController < ApplicationController
 
   def update
     @end_user = EndUser.find(params[:id])
-    @end_user.update
+    @end_user.update(end_user_params)
     redirect_to admin_end_user_path
+  end
+
+  private
+  def end_user_params
+    params.require(:end_user).permit(:last_name, :first_name, :last_name_kana, :first_name_kana, :email, :post_code, :address, :telephone_number, :is_delete)
   end
 end
