e5a5448e221c0300765326bcd60d9e7ac23ff91e
vagrant <vagrant@localhost.localdomain>
2021-02-07 12:31:09 +0000

    [fix]after-redirect

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,24 @@ class ApplicationController < ActionController::Base
     protect_from_forgery with: :exception
     before_action :configure_permitted_parameters, if: :devise_controller?
   
+    # ログイン後
+    def after_sign_in_path_for(resource)
+      if Admin
+        admin_root_path
+      else
+        root_path
+      end
+    end
+
+    # ログアウト後
+    def after_sign_out_path_for(resource)
+      if current_end_user
+        root_path
+      else
+        new_admin_session_path
+      end
+    end
+
     protected
   
     def configure_permitted_parameters
