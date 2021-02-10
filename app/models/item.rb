34c4b25fa20eda8039c54f840b3b801e7e89fea0
vagrant <vagrant@localhost.localdomain>
2021-02-10 06:48:08 +0000

    [add]itemimage

--- app/models/item.rb
+++ app/models/item.rb
@@ -2,4 +2,7 @@ class Item < ApplicationRecord
     belongs_to :genre
     has_many :order_details
     has_many :cart_items
+
+
+    attachment :image
 end
--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,4 @@
 #     https: false
 #   )
 # end
+Refile.secret_key = '5592d3afeb76e7a58857c21c81fa7446cc4759e4903a0b564d459acc69e4fbeaaca8523a043ea0494f7bbb429376f61c6052814fec50494d31f23b5e49464933'
