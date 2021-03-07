c7d4b7f52a3ec6c1c70aacf5a244a9f5e75d7280
vagrant <vagrant@localhost.localdomain>
2021-03-07 14:05:59 +0000

    [add]itemimage

---
+++ app/helpers/public/withdraw_helper 4.rb
@@ -0,0 +1,2 @@
+module Public::WithdrawHelper
+end
---
+++ app/models/item 2.rb
@@ -0,0 +1,8 @@
+class Item < ApplicationRecord
+    belongs_to :genre
+    has_many :order_details
+    has_many :cart_items
+
+
+    attachment :image
+end
